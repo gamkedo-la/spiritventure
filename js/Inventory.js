61018fd076e11369f23e953d93150fa7ab39942b
Ak-ab <68685683+Ak-ab@users.noreply.github.com>
2022-02-23 09:02:56 +0400

    put variables on teardrop specific inventory item to change text position relatively

--- js/Inventory.js
+++ js/Inventory.js
@@ -1,6 +1,7 @@
 const INVENTORY_ANIM_FRAME_WAIT = 15;
 const INVENTORY_ITEM_W = 50;
 const INVENTORY_ITEM_H = 50;
+const INVENTORY_FRAMES_TEARDROP = 3; 
 
 var showingInventory = false;
 var inventoryAnimFrame = 0; //use for all inventory animations
@@ -19,12 +20,14 @@ function drawAnimatedInventory(){
     var margin = 50;
     colorRect(margin, margin, canvas.width-margin*2, canvas.height-margin*2, "blue");
     canvasContext.globalAlpha = 1.0;
+    var teardropX = 40;
+    var teardropY = 65;
     canvasContext.drawImage(teardropAnim,
-        (inventoryAnimFrame%3)*INVENTORY_ITEM_W, 0, // source corner x, y
+        (inventoryAnimFrame%INVENTORY_FRAMES_TEARDROP)*INVENTORY_ITEM_W, 0, // source corner x, y
         INVENTORY_ITEM_W, INVENTORY_ITEM_H, //source width and height
-        25+margin, 25+margin, //dest corner x, y
+        teardropX+margin, teardropY+margin, //dest corner x, y
         INVENTORY_ITEM_W, INVENTORY_ITEM_H); //dest width and height
     canvasContext.fillStyle = "yellow";
-    canvasContext.fillText("teardrop", 25+margin, 50+margin+INVENTORY_ITEM_H);
+    canvasContext.fillText("teardrop", teardropX+margin, teardropY+25+margin+INVENTORY_ITEM_H);
 }
 
