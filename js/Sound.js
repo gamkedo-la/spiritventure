4486ff6c605e17cd490fd3c1ad8b26d539e655e6
Evan Sklarski <esklarski@gmail.com>
2022-03-16 01:28:38 -0700

    Sound Manager functions. Control volume levels, play sound effects, set bg music.

--- js/Sound.js
+++ js/Sound.js
@@ -1,58 +1,229 @@
-var audioFormat;
+/**
+ * Sound Manger
+ * 
+ * volume control: {@link volumeControl}
+ * 
+ * play bg music: {@link startBGM}
+ * 
+ * play sfx sound: {@link playSound}
+ * 
+ */
 
-function setFormat() {
-  var audio = new Audio();
-  if (audio.canPlayType("audio/mp3")) {
-      audioFormat = ".mp3";
-  } else {
-    audioFormat = ".ogg";
+/** Holds all Audio objects for easy looping. */
+let currentSFX = [];
+
+/**
+ * Volume Manger.
+ * Controls volume levels of various categories (currently: master, bgm, sfx) independantly
+ * 
+ * to set volume use:
+ * - masterVol(value)
+ * - bgmVol(value)
+ * - sfxVol(value)
+ * 
+ * values acepted are between 0 and 1
+ * 
+ * to toggle Mute use:
+ * - toggleMute()
+ */
+let volumeControl = {
+  // volume levels for query
+  master: 1,
+  bgm: 1,
+  sfx: 1,
+
+  // clamp revieved values
+  checkVolumeValue(value) {
+    if (value > 1) value = 1;
+    if (value < 0) value = 0;
+
+    return value;
+  },
+
+  get masterVol() {
+    return this.master;
+  },
+
+  // sets master volume level and updates all audio tracks
+  set masterVol(value) {
+    this.master = this.checkVolumeValue(value);
+
+    for (let track in currentSFX) {
+      currentSFX[track].volume = this.setVolume(track);
+    }
+  },
+
+  get bgmVol() {
+    return this.bgm;
+  },
+
+  // sets background sound volume and updates "bgm" category tracks
+  set bgmVol(value) {
+    this.bgm = this.checkVolumeValue(value);
+
+    for (let track in currentSFX) {
+      if (currentSFX[track].category == "bgm") {
+        currentSFX[track].volume = this.setVolume(track);
+      }
+    }
+  },
+
+  get sfxVol() {
+    return this.sfx;
+  },
+
+  // sets sound fx volume and updates "sfx" category tracks
+  set sfxVol(value) {
+    this.sfx = this.checkVolumeValue(value);
+
+    for (let track in currentSFX) {
+      if (currentSFX[track].category == "sfx") {
+        currentSFX[track].volume = this.setVolume(track);
+      }
+    }
+  },
+
+  toggleMute() {
+    for (let track in currentSFX) {
+      currentSFX[track].muted = !(currentSFX[track].muted);
+  
+      if (currentSFX[track].category == "bgm") {
+        if (currentSFX[track].muted == true) {
+          currentSFX[track].pause();
+        }
+        else {
+          currentSFX[track].play();
+        }
+      }
+    }
+  },
+
+  /** sets volume based on category */
+  setVolume(track) {
+    var volumeResult = 1;
+  
+    if (currentSFX[track].category == "bgm") {
+      volumeResult = volumeControl.bgm;
+    }
+    else if (currentSFX[track].category == "sfx") {
+      volumeResult = volumeControl.sfx;
+    }
+  
+    return currentSFX[track].volume =  volumeResult * this.master;
+  },
+}
+
+/**
+ * Loads Audio objects into currentSFX[] on demand,
+ * plays sound when/if loaded.
+ * 
+ * TODO: some error catching
+ */
+function sfx(fileName, loop = false) {
+  if (!currentSFX[fileName]) {
+    currentSFX[fileName] = new Audio("sound/" + fileName);
+  }
+
+  currentSFX[fileName].play();
+  currentSFX[fileName].volume = 1;
+  currentSFX[fileName].loop = loop;
+}
+
+/**
+ * Play looping track, on "bgm" category volume levels.
+ * Only allows one bgm track.
+ */
+function startBGM(track = "Lurking Sadness.mp3") {
+    // pause all previous bgm tracks
+    for (var thing in currentSFX) {
+      if (currentSFX[thing].category == "bgm") {
+        currentSFX[thing].currentTime = 0;
+        currentSFX[thing].pause();
+      }
+  }
+
+    sfx(track, true);
+    currentSFX[track].category = "bgm";
+    currentSFX[track].volume = volumeControl.setVolume(track);
+}
+
+/**
+ * Play looping track, on "sfx" category volume levels.
+ * Waits for track to finish before playing next.
+ */
+function playSound(track) {
+  if (!currentSFX[track]) {
+    sfx(track, false);
+    currentSFX[track].category = "sfx";
+    currentSFX[track].volume = volumeControl.setVolume(track)
+  }
+  else if (currentSFX[track].ended) {
+    currentSFX[track].play();
   }
 }
 
-function BackgroundMusicClass() {
 
-  var musicSound = null;
+
+
+
+// what is all this below?
+// ******************************************* //
+
+// var audioFormat;
+
+// function setFormat() {
+//   var audio = new Audio();
+//   if (audio.canPlayType("audio/mp3")) {
+//       audioFormat = ".mp3";
+//   } else {
+//     audioFormat = ".ogg";
+//   }
+// }
+
+// function BackgroundMusicClass() {
+
+//   var musicSound = null;
     
-  this.loopSong = function(filenameWithPath) {
-    setFormat(); // calling this to ensure that audioFormat is set before needed
+//   this.loopSong = function(filenameWithPath) {
+//     setFormat(); // calling this to ensure that audioFormat is set before needed
     
-    if(musicSound != null) {
-      musicSound.pause();
-      musicSound = null;
-    }
-    musicSound = new Audio(filenameWithPath+audioFormat);
-    musicSound.loop = true;
-    musicSound.play(); // this causes an error if the user didn't just click
-  }
+//     if(musicSound != null) {
+//       musicSound.pause();
+//       musicSound = null;
+//     }
+//     musicSound = new Audio(filenameWithPath+audioFormat);
+//     musicSound.loop = true;
+//     musicSound.play(); // this causes an error if the user didn't just click
+//   }
   
-  this.startOrStopMusic = function() {
-    if(musicSound.paused) {
-      musicSound.play();
-    } else {
-      musicSound.pause();
-    }
-  }
-}
+//   this.startOrStopMusic = function() {
+//     if(musicSound.paused) {
+//       musicSound.play();
+//     } else {
+//       musicSound.pause();
+//     }
+//   }
+// }
 
-function SoundOverlapsClass(filenameWithPath) { // accepting argument for constructor
+// function SoundOverlapsClass(filenameWithPath) { // accepting argument for constructor
   
-  setFormat(); // calling this to ensure that audioFormat is set before needed
+//   setFormat(); // calling this to ensure that audioFormat is set before needed
   
-  // All variables here are "private", hidden to outside. Use "var " - not "this."
-  var mainSound = new Audio(filenameWithPath+audioFormat);
-  var altSound = new Audio(filenameWithPath+audioFormat);
+//   // All variables here are "private", hidden to outside. Use "var " - not "this."
+//   var mainSound = new Audio(filenameWithPath+audioFormat);
+//   var altSound = new Audio(filenameWithPath+audioFormat);
 
-  var altSoundTurn = false;
+//   var altSoundTurn = false;
   
-  this.play = function() { // not "var ", keeping "this.", as we need it exposed!
-    if(altSoundTurn) { // note: no "this." before altSoundTurn since "var" local/private
-      altSound.currentTime = 0;
-      altSound.play();
-    } else {
-      mainSound.currentTime = 0;
-      mainSound.play();
-    }
-    altSoundTurn = !altSoundTurn; // toggle between true and false
-  }
+//   this.play = function() { // not "var ", keeping "this.", as we need it exposed!
+//     if(altSoundTurn) { // note: no "this." before altSoundTurn since "var" local/private
+//       altSound.currentTime = 0;
+//       altSound.play();
+//     } else {
+//       mainSound.currentTime = 0;
+//       mainSound.play();
+//     }
+//     altSoundTurn = !altSoundTurn; // toggle between true and false
+//   }
 
-}
+// }
