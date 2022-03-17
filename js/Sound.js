e65f74fa78ec4e52540f7aadef0809310f5f47f3
Evan Sklarski <esklarski@gmail.com>
2022-03-17 00:06:17 -0700

    Create quasi-enum to avoid typing errors with file names.

---
+++ js/Sound.js
@@ -0,0 +1,35 @@
+/**
+ * Quasi-enum of sound files for game.
+ * 
+ * use : playSound(Sound.LurkingSadness)
+ */
+const Sound = {
+    Cancel: "cancel.wav",
+    Confirm: "confirm.wav",
+    DialogAdvance: "dialog_advance.wav",
+    DialogSelect: "dialog_select.wav",
+    DialogStart: "dialog_start.wav",
+    DoorOpenDra: "door_open_dra.wav",
+    Drip: "drip.wav",
+    Grab: "grab.wav",
+    Haunt: "haunt.wav",
+    Heal: "heal.wav",
+    Hit: "hit.wav",
+    Hurt: "hurt.wav",
+    Jump: "jump.wav",
+    Low: "low.wav",
+    LurkingSadness: "lurking_sadness.mp3",
+    Ooh: "ooh.wav",
+    Open: "open.wav",
+    PickupKey: "pickup_key.mp3",
+    Pickup: "pickup.wav",
+    Play: "play.mp3",
+    Shiny: "shiny.wav",
+    ShrillNights: "shrillnights.wav",
+    Squeak: "squeak.wav",
+    Teleport: "teleport.wav",
+    Throw: "throw.wav",
+    Trigger: "trigger.wav",
+    Unlock: "unlock.wav",
+    Woosh: "woosh.wav",
+}
