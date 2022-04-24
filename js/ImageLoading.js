5e24dbba72e9dc505b43aa31a826072736de2e11
Ak-ab <68685683+Ak-ab@users.noreply.github.com>
2022-04-24 17:20:02 +0400

    Rework of Despond NPC art asset

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -38,7 +38,7 @@ function loadImages() {
     {tileType:TILE_KEY, theFile:"world_key.png"},
     {tileType:TILE_DOOR, theFile:"world_door.png"},
     {tileType:TILE_MIND, theFile:"heart.png"},
-    {tileType:TILE_DESPOND, theFile:"Despond_Lotus_(Brahma).png"},
+    {tileType:TILE_DESPOND, theFile:"Despond_Lotus.png"},
     {tileType:TILE_VASE, theFile:"sad_vase.png"},
     {tileType:TILE_GROUND2, theFile:"tile_marble1.png"},
     {tileType:TILE_GROUND3, theFile:"tile_marble2.png"},
