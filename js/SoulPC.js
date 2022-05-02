968f9030fd964af08b263fb124f6be4b33b51a80
Ak-ab <68685683+Ak-ab@users.noreply.github.com>
2022-05-02 18:39:39 +0400

    Despond convo works, previous commit broke a convo so I reverted it

--- js/SoulPC.js
+++ js/SoulPC.js
@@ -68,6 +68,7 @@ function warriorClass() {
 
     // notify re addition
     console.log("inventory item added");
+    canvasContext.fillText("Inventory item added.", 10, 90);
   }
 
   this.reset = function () {
@@ -249,7 +250,7 @@ function warriorClass() {
         var pos = getPixelCoordAtTileIndex(walkIntoTileIndex);
         wrapText = [testConvo2[0].text];
         lineWrap();
-        setupDialog(testconvo, pos[0] - (dialogW / 2) + (TILE_W / 2), pos[1] - (dialogHOffset + (dialogHPerLine * wrapText.length)) - (TILE_H * 2.4));
+        setupDialog(Despond, pos[0] - (dialogW / 2) + (TILE_W / 2), pos[1] - (dialogHOffset + (dialogHPerLine * wrapText.length)) - (TILE_H * 2.4));
         sfx('dialog_start.wav');
         break;
       case TILE_SATAH:
