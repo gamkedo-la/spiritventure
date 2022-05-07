a771a325f27043c623b7f849ea7c3688a693c055
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-05-07 18:46:54 +0500

    Update Main.js
    Practice Commit

--- js/Main.js
+++ js/Main.js
@@ -20,6 +20,7 @@ var rightDialogRoomFin = false;
 var belowDialogRoomFin = false;
 
 // game loop properties
+//Practice Commit
 var loadComplete = false;
 var gameloop;
 const framesPerSecond = 30;
